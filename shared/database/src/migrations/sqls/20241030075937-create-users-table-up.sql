d756b6d658029656d70e2edfdf0c799cc522b83d
phucpham <phuc.pxp@gmai.com>
2024-10-30 15:33:02 +0700

    refactor: clean up formatting in users table migration SQL

--- shared/database/src/migrations/sqls/20241030075937-create-users-table-up.sql
+++ shared/database/src/migrations/sqls/20241030075937-create-users-table-up.sql
@@ -3,18 +3,18 @@
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 CREATE TABLE users (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),           -- Khóa chính, UUID tự sinh
-    user_login VARCHAR(60) NOT NULL,                         -- Tên đăng nhập
-    user_pass VARCHAR(255) NOT NULL,                         -- Mật khẩu
-    user_nicename VARCHAR(50),                               -- Tên hiển thị
-    user_email VARCHAR(100) NOT NULL,                        -- Email
-    user_url VARCHAR(100),                                   -- URL cá nhân
-    user_registered TIMESTAMP NOT NULL,                      -- Thời gian đăng ký
-    user_activation_key VARCHAR(255),                        -- Khóa kích hoạt
-    user_status SMALLINT DEFAULT 0,                          -- Trạng thái người dùng
-    display_name VARCHAR(250),                               -- Tên hiển thị công khai
-    created_at TIMESTAMPTZ DEFAULT NOW(),                    -- Ngày tạo
-    updated_at TIMESTAMPTZ                                   -- Ngày cập nhật (sẽ được cập nhật tự động bởi trigger)
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),          
+    user_login VARCHAR(60) NOT NULL,                        
+    user_pass VARCHAR(255) NOT NULL,                         
+    user_nicename VARCHAR(50),                            
+    user_email VARCHAR(100) NOT NULL,                        
+    user_url VARCHAR(100),                                  
+    user_registered TIMESTAMP NOT NULL,                     
+    user_activation_key VARCHAR(255),                        
+    user_status SMALLINT DEFAULT 0,                          
+    display_name VARCHAR(250),                              
+    created_at TIMESTAMPTZ DEFAULT NOW(),                    
+    updated_at TIMESTAMPTZ                                  
 );
 
 CREATE OR REPLACE FUNCTION update_updated_at_column()
