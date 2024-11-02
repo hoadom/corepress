ef53fb8195e7c557f371da9af3e7972a11e18404
phuc.pham <phucpham.infinity@gmail.com>
2024-11-02 18:22:47 +0700

    chore: Update API routes and static assets configuration

--- packages/backend/email/index.ts
+++ packages/backend/email/index.ts
@@ -1,7 +1,7 @@
 import { api } from "encore.dev/api";
 
 export const get = api(
-  { expose: true, method: "GET", path: "/hello/:name" },
+  { expose: true, method: "GET", path: "/api/hello/:name" },
   async ({ name }: { name: string }): Promise<Response> => {
     const msg = `Hello ${name}!`;
     return { message: msg };
